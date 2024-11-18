663afc23f2db6d607f4fcdeeda26dc285cc8b541
B-Baumeister <bonnie.hoika@gmx.de>
2024-11-18 10:29:39 +0100

    add signal instead zone.js, updated code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../../dummy-users';
 
@@ -11,15 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // statt get imagePath benutzen wir jetzt "computed"
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //  this.selectedUser = DUMMY_USERS[randomIndex]; // das ist wird nicht mehr gebraucht, weil wir über set es jetzt updaten
   }
   //jetzt wird jedes mal ein neuer user geladen und ein neues image laden, sobald der button angeklickt wird; der randomindex wird dabei überschrieben
 }
