e3b3e585d83e4ab1b5abbc3b42844a00b0121148
B-Baumeister <bonnie.hoika@gmx.de>
2024-11-18 12:11:03 +0100

    add @Inputs for more flexibility and more li elements

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input, input } from '@angular/core'; //Input = Decorator; input= special function
 
-import { DUMMY_USERS } from '../../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,16 +8,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // statt get imagePath benutzen wir jetzt "computed"
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    //  this.selectedUser = DUMMY_USERS[randomIndex]; // das ist wird nicht mehr gebraucht, weil wir über set es jetzt updaten
+  get imagePath() {
+    return '../../assets/users/' + this.avatar;
   }
-  //jetzt wird jedes mal ein neuer user geladen und ein neues image laden, sobald der button angeklickt wird; der randomindex wird dabei überschrieben
+
+  onSelectUser() {}
 }
