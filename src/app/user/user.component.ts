f4c5c7568853ec526d85a9f6fdf968c2e36ecd2e
B-Baumeister <bonnie.hoika@gmx.de>
2024-11-22 13:11:36 +0100

    found bug --> in app-user [id]='users[0].avatar' instead of 'id'

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  computed,
-  EventEmitter,
-  Input,
-  input,
-  Output,
-  output,
-} from '@angular/core'; //Input = Decorator; input= special function
+import { Component, EventEmitter, Input, Output } from '@angular/core'; //Input = Decorator; input= special function
 
 @Component({
   selector: 'app-user',
@@ -19,7 +11,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter(); // "select" ist ein Event, das wir auslösen können, es wird als property für die funktion onSelectUser() verwendet
+  @Output() select = new EventEmitter<string>(); // "select" ist ein Event, das wir auslösen können, es wird als property für die funktion onSelectUser() verwendet
 
   // select = output<string>(); // generic type feature (<string>) behebt das Problem, dass der Typ nicht erkannt wird
 
