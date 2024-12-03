47c80ba63118a6ed5e4a46ddd3a339ec7461c97a
B-Baumeister <bonnie.hoika@gmx.de>
2024-12-03 15:22:36 +0100

    export user interface and task interface in seperate file

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'; //Input = Decorator; input= special function
-
-type User = { id: string; name: string; avatar: string };
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
